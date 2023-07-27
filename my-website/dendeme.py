aba1c29efdf560a856422970a659e67b80475af5
eyuphansahin1 <107874359+eyuphansahin1@users.noreply.github.com>
2023-07-27 08:56:36 +0300

    added

---
+++ my-website/dendeme.py
@@ -0,0 +1,9 @@
+print("Hesap makinesine hoşgeldiniz")
+
+birincisay  = int(input  ("bir sayı girin: "))
+ikincisayı  = int(input  ("bir sayı girin: " ))
+
+print(birincisayı + ikincisayı)
+
+print
+
